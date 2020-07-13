7f44318f615a12b552eafa0a37272835dff7f7a6
hth945 <599127999@qq.com>
2020-07-13 09:02:59 +0800

    添加文件与hash算法

--- skySoftware/include/Public_util.h
+++ skySoftware/include/Public_util.h
@@ -5,5 +5,7 @@
 
 #define OFFSETOF(TYPE, MEMBER) ((int)(&((TYPE *)0)->MEMBER))  //求偏移地址
 
+//一个简单的hash算法
+unsigned int hash_index(unsigned int *data, int len);
 #endif
 
--- skySoftware/src/Public_util.c
+++ skySoftware/src/Public_util.c
@@ -1,3 +1,14 @@
 #include "Public_util.h"
 
+//一个简单的hash算法
+unsigned int hash_index(unsigned int *data, int len)
+{
+	unsigned int h = 0;
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		h = h *31 + data[len];
+	}
+	return h;
+}
 
